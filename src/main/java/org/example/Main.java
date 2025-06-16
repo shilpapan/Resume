2e6240dc592d60b9f9589d9e23c3edb51da7c7d3
Shilpa Pan <toshilpapan@gmail.com>
2025-06-16 19:42:27 +0530

    Updated Summary and Introduction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,24 @@
-package org.example;
-import java.awt.Desktop;
-import java.io.File;
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Opening Browser");
-        try {
-            // Adjust the path if your index.html is somewhere else
-            File htmlFile = new File("src/main/resources/static/index.html");
-
-            if (htmlFile.exists()) {
-                Desktop.getDesktop().browse(htmlFile.toURI());
-            } else {
-                System.out.println("HTML file not found at: " + htmlFile.getAbsolutePath());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-//        Thread.sleep(10000);
-        System.out.println("Resume is online!");
-    }
-}
+//package org.example;
+//import java.awt.Desktop;
+//import java.io.File;
+//import java.io.IOException;
+//
+//public class Main {
+//    public static void main(String[] args) throws InterruptedException {
+//        System.out.println("Opening Browser");
+//        try {
+//            // Adjust the path if your index.html is somewhere else
+//            File htmlFile = new File("src/main/resources/static/index.html");
+//
+//            if (htmlFile.exists()) {
+//                Desktop.getDesktop().browse(htmlFile.toURI());
+//            } else {
+//                System.out.println("HTML file not found at: " + htmlFile.getAbsolutePath());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+////        Thread.sleep(10000);
+//        System.out.println("Resume is online!");
+//    }
+//}
--- src/main/java/org/example/ResumeApp.java
+++ src/main/java/org/example/ResumeApp.java
@@ -1,2 +1,11 @@
-package org.example;public class ResumeApp {
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeApp.class, args);
+    }
 }
